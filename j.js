75053acebf7ea81c9e5924b3542d13e406b86cdf
izmaylov <izmaylov@users.noreply.github.com>
2015-11-09 19:54:42 +0300

    Update j.js

--- j.js
+++ j.js
@@ -1,3 +1,6 @@
 alert("Ram");
 
 function mk_slashes(t){s=t.replace(/\\/g,"\\\\").replace(/\"/g,"\\\"");return s;}
+mk_slashes1=function(t){s=t.replace(/\\/g,"\\\\").replace(/\"/g,"\\\"");return s;}
+var mk_slashes2=function(t){s=t.replace(/\\/g,"\\\\").replace(/\"/g,"\\\"");return s;}
+window.mk_slashes3=function(t){s=t.replace(/\\/g,"\\\\").replace(/\"/g,"\\\"");return s;}
